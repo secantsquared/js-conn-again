559b5ee51f0f6862a48212ff2a7fb83ab316cc5e
ryanboris <ryan.boris@gmail.com>
2019-03-14 07:16:29 -0400

    deploys

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 creds = {
-    'CLIENT_ID': 'cef50287b44defde401b',
-    'CLIENT_SECRET': 'ebd6a6b67e160f9cbcdfe921a1dde47610edaa3f'
+    'CLIENT_ID': 'd420a1741efb256cb487',
+    'CLIENT_SECRET': 'b1b1bbbe1a57ee8f6cec50f17f5d932fb908ffb2'
 }
